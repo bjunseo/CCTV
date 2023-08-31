8006af6416b924403adc8a27c694a0cd9a9c3484
bjunseo <junseo010234@gmail.com>
2023-08-31 18:52:32 +0900

    ADD camera streaming ^_^

--- CCTV_Server/CCTV_Server/Form1.Designer.cs
+++ CCTV_Server/CCTV_Server/Form1.Designer.cs
@@ -28,21 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.camDisplay = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.camDisplay)).BeginInit();
             this.SuspendLayout();
+            // 
+            // camDisplay
+            // 
+            this.camDisplay.Location = new System.Drawing.Point(12, 40);
+            this.camDisplay.Name = "camDisplay";
+            this.camDisplay.Size = new System.Drawing.Size(1332, 568);
+            this.camDisplay.TabIndex = 0;
+            this.camDisplay.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(144, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 21);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(126, 21);
+            this.textBox1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1356, 620);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.camDisplay);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.camDisplay)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox camDisplay;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- CCTV_Server/CCTV_Server/Form1.cs
+++ CCTV_Server/CCTV_Server/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -11,6 +12,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
 
+using OpenCvSharp;
+using System.IO;
+using System.Threading;
 
 namespace CCTV_Server
 {
@@ -24,6 +28,16 @@ namespace CCTV_Server
         List<Socket> connectedClients = new List<Socket>();
 
         int m_port = 5000;
+
+        Thread frameDecodeThread;
+
+        // After adding the DLL to the project, change the "Copy to Output Directory" value to "Copy If newer".
+        string dllPath = Path.Combine(Environment.CurrentDirectory, "FFmpeg.AutoGen", "bin", "x64");
+
+        string rtspAddr = "rtsp://admin:dmenc001!@192.168.0.250:554/ISAPI/streaming/channels/101";
+        string httpAddr = "http://admin:dmenc001!@192.168.0.250:80/ISAPI/Streaming/channels/102/httpPreview";
+        string url = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4";
+
         #endregion
 
         #region socketStart 소켓을 연다
@@ -59,6 +73,11 @@ namespace CCTV_Server
         {
             InitializeComponent();
 
+            frameDecodeThread = new Thread(new ThreadStart(Run_frameDecodeThread));
+            frameDecodeThread.IsBackground = true;
+            FFmpegBinariesHelper.RegisterFFmpegBinaries(dllPath);
+
+            frameDecodeThread.Start();
         }
         #endregion
 
@@ -125,5 +144,64 @@ namespace CCTV_Server
         }
         #endregion
 
+
+        #region 카메라쪽 코드
+        private unsafe void Run_frameDecodeThread()
+        {
+            using (var decoder = new VideoStreamDecoder(rtspAddr))
+            {
+                var srcSize = decoder.FrameSize;
+                var pixelFormat = decoder.PixelFormat;
+                var destSize = srcSize;
+                var destPixelFormat = FFmpeg.AutoGen.Abstractions.AVPixelFormat.AV_PIX_FMT_BGR24;
+
+                using (var frameConverter = new VideoFrameConverter(srcSize, pixelFormat, destSize, destPixelFormat))
+                {
+                    while (decoder.TryDecodeNextFrame(out var frame))
+                    {
+                        var convertedframe = frameConverter.Convert(frame);
+                        var bitmap = new Bitmap(convertedframe.width, convertedframe.height,
+                            convertedframe.linesize[0], PixelFormat.Format24bppRgb, (IntPtr)convertedframe.data[0]);
+
+                        if (camDisplay.InvokeRequired)
+                        {
+                            camDisplay.Invoke(new MethodInvoker(delegate {
+                                camDisplay.Image = bitmap;
+                            }));
+                        }
+                        else camDisplay.Image = bitmap;
+                    }
+                }
+            }
+        }
+
+        private unsafe Mat AVframeToMat(FFmpeg.AutoGen.Abstractions.AVFrame* frame)
+        {
+            int width = frame->width, height = frame->height;
+            int[] lSize = new int[1];
+            byte*[] pByteArr = new byte*[1];
+
+            Mat mat, tmpMat = new Mat(height, width, MatType.CV_8UC3);
+            FFmpeg.AutoGen.Abstractions.SwsContext* conversion;
+            pByteArr[0] = tmpMat.DataPointer;
+            lSize[0] = (int)tmpMat.Step1();
+
+            conversion = FFmpeg.AutoGen.Abstractions.ffmpeg.sws_getContext(
+                width, height, (FFmpeg.AutoGen.Abstractions.AVPixelFormat)frame->format, width, height,
+                FFmpeg.AutoGen.Abstractions.AVPixelFormat.AV_PIX_FMT_BGR24, FFmpeg.AutoGen.Abstractions.ffmpeg.SWS_BICUBIC,
+                null, null, null);
+            FFmpeg.AutoGen.Abstractions.ffmpeg.sws_scale(conversion, frame->data, frame->linesize, 0, height, pByteArr, lSize);
+            mat = new Mat(height, width, MatType.CV_8UC3, (IntPtr)pByteArr[0]);
+
+            FFmpeg.AutoGen.Abstractions.ffmpeg.sws_freeContext(conversion);
+            return mat;
+        }
+
+        #endregion
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            frameDecodeThread.Abort();
+        }
     }
 }
